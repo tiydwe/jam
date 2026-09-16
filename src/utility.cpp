91157f452db076d4a94c9ab1b15170617ceacd96
tiydwe <tiydwe@gmail.com>
2026-09-16 12:14:21 -0700

    Fixed some bugs

--- src/utility.cpp
+++ src/utility.cpp
@@ -3,12 +3,14 @@
 #include <cstdlib>
 #include <iostream>
 
-size_t utility::uid() {
+size_t utility::uid()
+{
   static size_t curr = 0;
   return curr++;
 }
 
-void utility::registerPhysicalID(size_t id) {
+void utility::registerPhysicalID(size_t id)
+{
   _maxPhysicalID = std::max(_maxPhysicalID, id);
 }
 
@@ -16,62 +18,105 @@ size_t utility::getNewPhysicalID() { return ++_maxPhysicalID; }
 
 void utility::logErr(std::string msg) { std::cerr << "ERROR: " << msg << "\n"; }
 
-void utility::logWarn(std::string msg) {
+void utility::logWarn(std::string msg)
+{
   std::cerr << "WARNING: " << msg << "\n";
 }
 
 void utility::log(std::string msg) { std::cout << "INFO: " << msg << "\n"; }
 
-void utility::exit(std::string msg, int code) {
+void utility::exit(std::string msg, int code)
+{
   std::cerr << "EXITING DUE TO: " << msg << "\n";
   throw msg;
   // std::exit(code);
 }
 
-double utility::smoothstep(double x) {
+double utility::smoothstep(double x)
+{
   x = std::min(1.0, std::max(0.0, x));
   return 6 * x * x * x * x * x - 15 * x * x * x * x + 10 * x * x * x;
 }
 
-double utility::cumlitivePtoSlice(double (*p)(double), double t, double dt) {
+double utility::cumlitivePtoSlice(double (*p)(double), double t, double dt)
+{
   return 1.0 - (1.0 - p(t + dt)) / (1.0 - p(t));
 }
 
-std::filesystem::path utility::getSaveFileDirectory() {
-    std::filesystem::path saveDirectory;
-    std::string folderName = "jam_game";
+std::filesystem::path utility::getSaveFileDirectory()
+{
+  std::filesystem::path saveDirectory;
+  std::string folderName = "jam_game";
 #if defined(_WIN32)
-  const char* appData = std::getenv("APPDATA");
-  if (appData) {
+  const char *appData = std::getenv("APPDATA");
+  if (appData)
+  {
     saveDirectory = std::filesystem::path(appData) / folderName;
-  } else {
+  }
+  else
+  {
     saveDirectory = std::filesystem::current_path() / "saves";
   }
 #elif defined(__APPLE__)
-  const char* home = std::getenv("HOME");
-  if (home) {
+  const char *home = std::getenv("HOME");
+  if (home)
+  {
     saveDirectory = std::filesystem::path(home) / "Library" / "Application Support" / folderName;
   }
 #else
-  const char* xdgData = std::getenv("XDG_DATA_HOME");
-  if (xdgData) {
+  const char *xdgData = std::getenv("XDG_DATA_HOME");
+  if (xdgData)
+  {
     saveDirectory = std::filesystem::path(xdgData) / folderName;
-  } else {
-    const char* home = std::getenv("HOME");
-    if (home) {
+  }
+  else
+  {
+    const char *home = std::getenv("HOME");
+    if (home)
+    {
       saveDirectory = std::filesystem::path(home) / ".local" / "share" / folderName;
     }
   }
 #endif
-  if(!saveDirectory.empty() && !std::filesystem::exists(saveDirectory)){
+  if (!saveDirectory.empty() && !std::filesystem::exists(saveDirectory))
+  {
     std::filesystem::create_directory(saveDirectory);
   }
   return saveDirectory;
 }
 
-std::filesystem::path utility::getSaveFilePath(const std::string& gameName,
-                                               const std::string& fileName) {
+std::filesystem::path utility::getSaveFilePath(const std::string &gameName,
+                                               const std::string &fileName)
+{
   return getSaveFileDirectory() / gameName / fileName;
+}
+
+std::string utility::truncateText(sf::Text &textObj, const std::string &str, float maxWidth)
+{
+  textObj.setString(str);
+  if (textObj.getLocalBounds().size.x <= maxWidth)
+  {
+    return str;
+  }
+
+  std::string ellipsis = "...";
+  textObj.setString(ellipsis);
+  float ellipsisWidth = textObj.getLocalBounds().size.x;
+
+  float targetWidth = maxWidth - ellipsisWidth;
+
+  std::string truncated = "";
+  for (size_t i = 0; i < str.size(); ++i)
+  {
+    textObj.setString(truncated + str[i]);
+    if (textObj.getLocalBounds().size.x > targetWidth)
+    {
+      break;
+    }
+    truncated += str[i];
+  }
+
+  return truncated + ellipsis;
 }
 
 const float utility::Constants::GREEN_PHASE_TIME_DEFAULT = 20;

8371248c3b73a714ac42aa1db00290abc5c9f964
tiydwe <tiydwe@gmail.com>
2026-09-16 21:13:34 -0700

    Finished core UI

---
+++ include/game/TitleWindow.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "Button.h"
+#include "utility.h"
+
+class Game;
+
+class TitleWindow : public sf::Drawable, sf::Transformable {
+ public:
+  TitleWindow(Game* game, sf::Vector2f size);
+  void updateWindowSize(sf::Vector2f newSize);
+  void update(sf::Vector2f mousePos);
+  virtual void draw(sf::RenderTarget& target,
+                    sf::RenderStates states) const override;
+
+ private:
+  Game* _game;
+  sf::RectangleShape _contentBox;
+  sf::Text _title;
+  Button _load;
+  Button _newLevel;
+};
